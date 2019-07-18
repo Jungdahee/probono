76e47620013346d9abdfc4f943155041ba9e4214
tenid <suj710101@gmail.com>
2019-07-19 03:31:37 +0900

    알람 발생시 화면 켜짐

--- AndroidStudioProjectsMedicineAlarm/app/src/main/java/com/example/medicinealarm/AlarmControl.java
+++ AndroidStudioProjectsMedicineAlarm/app/src/main/java/com/example/medicinealarm/AlarmControl.java
@@ -4,11 +4,13 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class AlarmControl extends AppCompatActivity {
@@ -26,8 +28,6 @@ public class AlarmControl extends AppCompatActivity {
                 PendingIntent.FLAG_CANCEL_CURRENT);
 
         alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-
 
 
         alarmCancle = (Button)findViewById(R.id.alarmCancel);
